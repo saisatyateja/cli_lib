6d968b05e116474345713c8cbd9ec9092cd56ed2
saisatyateja <saisatyatejagunisetti@gmail.com>
2022-10-28 16:00:08 +0530

    shared library

--- vars/aws-cli.groovy
+++ vars/aws-cli.groovy
@@ -2,15 +2,19 @@ def call() {
     pipeline{
     agent{label 'shopizer'}
     stages{
-       // stage('vcs'){
-        //    steps{
-         //       git url:'https://github.com/saisatyateja/cli_lib.git',
+        stage('vcs'){
+        steps{
+            script{
+                git url:'https://github.com/saisatyateja/cli_lib.git',
                     branch:'main'
-         //   }
-       // }
+            }
+           }
+        }
         stage('build'){
             steps{
-                sh 'bash aws_cli_v0.1.sh'
+                script{
+                  sh 'bash aws_cli_v0.1.sh'
+                }
             }
         }
     }
